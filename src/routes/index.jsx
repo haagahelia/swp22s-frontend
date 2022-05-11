640987fd31e207aa9d17007244a267945987f141
ritamiklan <51336470+ritamiklan@users.noreply.github.com>
2022-05-11 11:14:23 +0300

    view boilerplate and routing added

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -14,6 +14,7 @@ import SignaturesView from '../views/SignaturesView'
 import SignatureView from '../views/SignatureView'
 import SignView from '../views/SignView'
 import CreateNewOrder from '../views/CreateNewOrder'
+import UnsignedTimelimitView from '../views/UnsignedTimelimitView'
 
 export default function AppRoutes() {
     return (
@@ -23,6 +24,7 @@ export default function AppRoutes() {
                 <Route path="/order/:orderId" element={<OrderView />} />
                 <Route path="/unsigned" element={<UnsignedOrdersView />} />
                 <Route path="/signatures" element={<SignaturesView />} />
+                <Route path="/unsignedtimelimit" element={<UnsignedTimelimitView/>}/>
                 <Route path="/signatures/:id" element={<SignatureView />} />
                 <Route path="/sign/:orderId" element={<SignView />} />
                 <Route path="/new" element={<CreateNewOrder />} />
--- src/views/UnsignedTimelimitView.jsx
+++ src/views/UnsignedTimelimitView.jsx
@@ -7,3 +7,48 @@
 // 4) publishing it to the view
 
 // also add this view to routing
+
+//copied stuff from the UnsignedORdersView from now
+
+import React, { useState, useEffect } from "react"
+import { Typography } from "@mui/material"
+import dao from "../ajax/dao"
+
+import { usePopup } from "../contexts/PopupContext"
+import { useOrder } from "../contexts/OrderContext"
+import Container from "../components/CommonComponents/Container"
+import OrderTable from "../components/OrderTable/Table"
+
+export default function UnsignedTimelimitView() {
+    const { setContent } = usePopup()
+    const { setOrder } = useOrder()
+    const [ unsigned, setUnsigned ] = useState([])
+
+    useEffect(() => {
+        fetchUnsignedOrders()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    const fetchUnsignedOrders = async () => {
+        try {
+            const data = await dao.getUnsignedOrders();
+            console.log("Data: " + data);
+            const dataWithId = data.map((d, i) => {
+                return { ...d, id: i }
+            })
+
+            setUnsigned(dataWithId)
+            setContent({ isOpen: true, msg: "Successfully fetched unsigned orders" })
+        } catch (error) {
+            console.log(error);
+            setContent({ isOpen: true, msg: `Can't fetch unsigned orders, ${error}` })
+        }
+    };
+
+    return (
+        <Container>
+            <Typography variant="h5" fontWeight="bold" my={2} color="primary">Unsigned Orders</Typography>
+            <OrderTable orders={unsigned} setOrder={setOrder} /> 
+        </Container>
+    )
+}
