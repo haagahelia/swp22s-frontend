80e46eeb8105ebc3b5776f529825a26d6ab28fa9
miikke79 <74132243+miikke79@users.noreply.github.com>
2022-05-15 23:38:48 +0300

    tidy up code

--- src/components/OrderTable/Delete.jsx
+++ src/components/OrderTable/Delete.jsx
@@ -3,20 +3,16 @@ import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import Button from '@mui/material/Button';
 import dao from "../../ajax/dao";
 import { usePopup } from "../../contexts/PopupContext";
-import { useNavigate } from "react-router-dom";
 
 export default function Delete(props) {
     const { setContent } = usePopup();
-    const navigate = useNavigate()
     const rowData = props.valueFormatted ? props.valueFormatted : props.value
 
     const deleteRow = async () => {
         if (window.confirm('Wait!... Are you sure you want to delete this order?')) {
             try {
                 await dao.deleteOrder(`${rowData.uuid}`);
-                dao.getOrders()
                 setContent({ isOpen: true, msg: `Order has been deleted!` });
-                navigate("/");
             } catch (error) {
                 setContent({ isOpen: true, msg: `Can't delete the order, ${error}` });
             }
